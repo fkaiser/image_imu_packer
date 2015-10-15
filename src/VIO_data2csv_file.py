56827cf3ebb68db39fbcbae53be9bed5208b319c
fkaiser <fkaiser@student.ethz.ch>
2015-10-15 18:10:10 +0200

    Customized version save to csv

--- src/VIO_data2csv_file.py
+++ src/VIO_data2csv_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
+import argparse
+import sys
 import rosbag
 import csv
 import os
@@ -8,24 +10,33 @@ from vio_ros.msg import vio_vis
 
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Saves output pose data from VIO in csv file',
+                                     prog='rosrun img_imu_packer VIO_data2csv.py')
+	parser.add_argument('--input_bag', '-i', action='store', type=str, nargs=1, help='The source bag to read from. Must contain vio_ros messages')
+	parser.add_argument('--VIO_topic', '-v', action='store', type=str, nargs=1, default=['/vio_vis/vio_vis'], help='VIO package topic name')
+	parser.add_argument('--csv_file', '-c', action='store', type=str, nargs=1, help='CSV file name with path')
+	args = parser.parse_args()
+	
+	print('Input bag: {}'.format(args.input_bag[0]))
+	in_bag = rosbag.Bag(args.input_bag[0], 'r')
+	#print(in_bag)
+	
+	print('VIO input msg topic name: {}'.format(args.VIO_topic[0]))
+	vio_topic_name_ = args.VIO_topic[0]
+	
+	print('Save data to: {}'.format(args.csv_file[0]))
+	path_csv_ = args.csv_file[0]
+	
+	
 	tmp_message=vio_vis()
-	pathname_='/newfs/bagfiles/output_VIO/'
-	bagname_='start1.bag'
-	filename_='start1.csv'
-	total_path_bag_=os.path.join(pathname_,bagname_)
-	topic_name_='/vio_vis/vio_vis'
-	
-	# Open bag file
-	in_bag = rosbag.Bag(total_path_bag_,'r')
-	path_csv_=os.path.join(pathname_,filename_)
-	
 	# Create csv file
 	with open(path_csv_, 'w+') as csvfile:
 		mycsv_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		
 		# Look in bag file and save robot pose and time stamp
 		#spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-		for topic, msg, t in in_bag.read_messages(topics=[topic_name_]):
-	
+		for topic, msg, t in in_bag.read_messages(topics=[vio_topic_name_]):
+		
 			mycsv_writer.writerow([msg.image.header.stamp.secs,msg.image.header.stamp.nsecs,msg.robot_pose.position.x,msg.robot_pose.position.y,msg.robot_pose.position.z,msg.robot_pose.orientation.x,msg.robot_pose.orientation.y,msg.robot_pose.orientation.z,msg.robot_pose.orientation.w])
 
 	
--- src/pack_show_save_VIO.sh
+++ src/pack_show_save_VIO.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-input_bagfile_=/newfs/bagfiles/2015-08-29-12-32-32.bag
-VIO_inputdata_bagfile_=/newfs/bagfiles/image_imu_packages/2015-08-29-12-32-32_VIO.bag
-VIO_outputdata_bagfile_=/newfs/bagfiles/image_imu_packages/Viooutput.bag
+bagname_=2015-08-29-12-32-32
+input_bagfile_="/newfs/bagfiles/${bagname_}.bag"
+VIO_inputdata_bagfile_="/newfs/bagfiles/image_imu_packages/${bagname_}_VIO_input.bag"
+VIO_outputdata_bagfile_="/newfs/bagfiles/output_VIO/${bagname_}_VIO_output.bag"
+CSV_filename_="/newfs/bagfiles/output_VIO/${bagname_}_VIO_output.csv"
+Vio_bag_rate_=0.2
+Secs_in_bag_=12
+
 vio_inpute_pack=0
 run_vio=1
 record_vio=0
-Vio_bag_rate_=0.5
-Secs_in_bag_=12
+transfer_to_csv=0
+kill_nodes=0
 
 
 
@@ -29,7 +34,7 @@ roslaunch vio_ros vis_no_images.launch &
 rosrun rqt_image_view rqt_image_view &
 
 if [ $record_vio -ge 1 ]; then
-rosbag record /vio_vis/rviz/robot_pose /vio_vis/rviz/robot_path /vio_vis/vio_vis -O & $VIO_outputdata_bagfile_
+rosbag record /vio_vis/rviz/robot_pose /vio_vis/rviz/robot_path /vio_vis/vio_vis -O $VIO_outputdata_bagfile_ & 
 fi
 
 # Sleep for certain amount such that node will have been started
@@ -37,13 +42,18 @@ sleep 4
 
 # Start rosbag with VIO input data
 rosbag play --pause --start=$Secs_in_bag_ --rate=$Vio_bag_rate_ $VIO_inputdata_bagfile_
+fi
 
+# Kill all ros nodes
+if [ $kill_nodes -ge 1 ]; then
+sleep 2
+rosnode kill --all
+fi
+
+if [ $transfer_to_csv -ge 1 ]; then
+rosrun img_imu_packer VIO_data2csv_file.py --input_bag $VIO_outputdata_bagfile_ --csv_file $CSV_filename_
 
 fi
-# Start recording of VIO data
-#record_VIO_data $3
-# Start rosbag file 
-#rosbag play  
 
 
 
